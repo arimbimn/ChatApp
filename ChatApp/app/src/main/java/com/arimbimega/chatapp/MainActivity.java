ade37314501e5c40ed8e13bfe226f9a84c9edcda
churroo <pradyaanti@gmail.com>
2021-06-16 22:38:07 +0700

    mencoba mengirim notification melalui console fcm

--- ChatApp/app/src/main/java/com/arimbimega/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/arimbimega/chatapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.arimbimega.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "Failed Get Token: ", task.getException());
+                            return;
+                        }
+
+                        //Get new FCM Token
+                        String token = task.getResult();
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                    }
+                });
     }
 }
--- ChatApp/app/src/main/java/com/arimbimega/chatapp/MyFirebaseMessagingService.java
+++ ChatApp/app/src/main/java/com/arimbimega/chatapp/MyFirebaseMessagingService.java
@@ -3,7 +3,12 @@ package com.arimbimega.chatapp;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -13,6 +18,28 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onNewToken( String token) {
         super.onNewToken(token);
 
+        Log.d(TAG, "Refreshed token: " + token);
+
         //Log.d(TAG, "Get Token : " + token);
     }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+
+        if ( remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+
+        if ( remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message notification body: " + remoteMessage.getNotification().getBody());
+        }
+
+    }
+
+
 }
